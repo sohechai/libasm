651da6aa745cb94264497bb5c377dc79807da64b
Sofia Hechaïchi <sofiahechaichi@MacBook-Pro.local>
2020-12-20 17:54:35 +0100

    modif strcmp

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,25 +1,20 @@
 section .text
-		global	ft_strcmp
+        global    ft_strcmp
 ft_strcmp:
-	mov rax, 0					;rax = 0
+    mov rax, 0                    ;rax = 0
 start:
-	mov	al, BYTE[rdi]			;al = *dest
-	mov	bl, BYTE[rsi]			;b = *src
-	cmp	al, 0					;al ? '\0'
-	je	end						; = '\o' -> end
-	cmp	bl, 0					;bl ? '\0'
-	je	end						;= '\0' -> end
-	cmp	al, bl					; al ? bl
-	jb	neg
-	ja	pos						; al > bl -> sub
-	inc	rdi						; *rdi ++
-	inc	rsi						; *rsi ++
-	jmp start					; loop
-pos:
-	mov rax, 1					;rax = 1
-	jmp	end						;-> end
-neg:
-	mov	rax, -1					; rax = -1
-	jmp	end						;-> end
+    mov    al, BYTE[rdi]            ;al = *dest
+    mov    bl, BYTE[rsi]            ;bl = *src
+    cmp    al, 0                    ;al ? '\0'
+    je    end                        ; = '\o' -> end
+    cmp    bl, 0                    ;bl ? '\0'
+    je    end                        ;= '\0' -> end
+    cmp    al, bl                    ;al ? bl
+    jne    end                        ; al != bl
+    inc    rdi                        ; *rdi ++
+    inc    rsi                        ; *rsi ++
+    jmp start                    ; loop
+
 end:
-	ret							; return rax
+    sub rax, rbx                ;rax - rbx = (ascii int that we obtain at the moment we decide to call end : 2 different char, or end of one string )
+    ret
